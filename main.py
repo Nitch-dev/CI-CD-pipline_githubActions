fd66d34cd8279a7ff5609b8bfabaa9ce81b6e0e1
Nitch <hassan.rahmani922@gmail.com>
2022-06-17 10:10:18 +0500

    customized main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world from the file!!")
+print("welcome to the Game inside the docker!!")
+
+user = input("> ")
+
+if user == "docker":
+    print("so you are inside docker thats crazy!!")
+
+else:
+    print("so it failed! i kinda was hoping it to fail")
